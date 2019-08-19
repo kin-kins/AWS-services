0219b5db19382eb5da5d61894eca82dc47daef50
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2019-08-19 15:34:03 +0530

    Update aws_kinesis_get_record.py

--- aws_kinesis_get_record.py
+++ aws_kinesis_get_record.py
@@ -1,47 +1,44 @@
 import boto3
 import json
+from datetime import datetime
+import time
 import base64
-
-my_stream_name = 'new_try'
-
-kinesis_client = boto3.client('kinesis', region_name='us-east-1')
-
-def put_to_stream(thing_id,b64,count):
-    payload = {
-                'image_id': str(b64),
-                #'image_id': count
-              }
-
-    #print (payload)
-    put_response = kinesis_client.put_record(
-                        StreamName=my_stream_name,
-                        Data=json.dumps(payload),
-                        PartitionKey=thing_id)
-    print (put_response)
 
 
 def main():
-    image_id = 0
+    my_stream_name = 'new_try'
 
-    while True:
-        thing_id = 'CAMERA-001'
-        with open("/Users/ashukumar/Desktop/Standard/ASHUimgs/"+str(image_id)+".jpg","rb") as imageFile:
-            stream = base64.b64encode(imageFile.read())
-        # print(stream)
-        # t=str(stream)
-        # t=t[2:-1]
-        # print('\n')
-        # print(t)
-        # fh = open("/Users/ashukumar/Desktop/kin/" + str(image_id) + ".jpg", "wb")
-        # fh.write(base64.b64decode(t))
-        # fh.close()
-        #print("/Users/ashukumar/Desktop/Standard/ASHUimgs/" + str(image_id) + ".jpg")
-        #print(stream)
-        put_to_stream(thing_id,stream,image_id)
-        image_id+=1
-        #print(image_id)
-        if image_id>389:
-            break
+    kinesis_client = boto3.client('kinesis', region_name='us-east-1')
 
-if __name__=='__main__' :
+    response = kinesis_client.describe_stream(StreamName=my_stream_name)
+
+    my_shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+
+
+    shard_iterator = kinesis_client.get_shard_iterator(StreamName=my_stream_name,
+                                                       ShardId=my_shard_id,
+                                                       ShardIteratorType='AT_SEQUENCE_NUMBER',StartingSequenceNumber='49598685317897772500072500026065375050514518896113876994',)
+
+    my_shard_iterator = shard_iterator['ShardIterator']
+
+    record_response = kinesis_client.get_records(ShardIterator=my_shard_iterator)
+    records=(record_response['Records'])
+    count=0
+    for record in records:
+        t_record=(str(record['Data'])[16:-5]+'\'')
+        t_record=t_record[0:1]+t_record[2:]
+        t_record=t_record[2:-1]
+        #print(t_record)
+       # print("\n")
+        fh = open("/Users/ashukumar/Desktop/kin/"+str(count)+".jpg", "wb")
+        fh.write(base64.b64decode(t_record))
+        fh.close()
+        count+=1
+    # while 'NextShardIterator' in record_response:
+    #     record_response = kinesis_client.get_records(ShardIterator=record_response['NextShardIterator'],Limit=2)
+    #     print (record_response)
+    # wait for 5 seconds
+        #time.sleep(5)
+
+if __name__ == '__main__':
     main()
